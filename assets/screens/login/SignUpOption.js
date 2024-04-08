50ed7855d5cb37f6f290d68062087516fc4cea10
Jacob9606 <snm9606@gmail.com>
2024-04-08 23:27:17 +1000

    0804

---
+++ assets/screens/login/SignUpOption.js
@@ -0,0 +1,60 @@
+import React from "react";
+import { View, Text, StyleSheet, TouchableOpacity, Image } from "react-native";
+
+const SignUpOption = ({ navigation }) => {
+  const handleCustomerSignup = () => {
+    // Navigate to the Customer Signup Screen
+    console.log("Navigate to Customer Signup");
+  };
+
+  const handleOwnerSignup = () => {
+    // Navigate to the Restaurant Owner Signup Screen
+    console.log("Navigate to Owner Signup");
+  };
+
+  return (
+    <View style={styles.container}>
+      <Image
+        source={require("../images/FoodFleetsLogo.png")}
+        style={styles.logo}
+      />
+      <TouchableOpacity style={styles.button} onPress={handleCustomerSignup}>
+        <Text style={styles.buttonText}>Customer</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.button} onPress={handleOwnerSignup}>
+        <Text style={styles.buttonText}>Restaurant Owner</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#fff",
+    padding: 20,
+  },
+  logo: {
+    width: 150, // Adjust to fit your logo's aspect ratio
+    height: 150, // Adjust to fit your logo's aspect ratio
+    resizeMode: "contain",
+    marginBottom: 30,
+  },
+  button: {
+    backgroundColor: "#FFD700", // Golden yellow color
+    paddingVertical: 15,
+    paddingHorizontal: 30,
+    borderRadius: 30,
+    marginVertical: 10,
+    width: "80%",
+    alignItems: "center",
+  },
+  buttonText: {
+    color: "#000", // Black color text
+    fontSize: 18,
+  },
+});
+
+export default SignUpOption;
